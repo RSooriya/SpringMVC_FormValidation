54b0e9a3e235b18750a5d11a96610b35ef79d9c7
RSooriya <soormo06@gmail.com>
2020-04-24 22:12:23 +0530

    Commit made for @InitBinder to preprocess each request

--- src/com/pack/sooriya/CustomerController.java
+++ src/com/pack/sooriya/CustomerController.java
@@ -2,14 +2,24 @@ package com.pack.sooriya;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomerController {
+	
+	@InitBinder
+	public void binderCall(WebDataBinder binder)
+	{
+		StringTrimmerEditor editor = new StringTrimmerEditor(true);
+		binder.registerCustomEditor(String.class, editor);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model model)
